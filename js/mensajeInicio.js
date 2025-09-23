08c87032dd7f2d6ebd41232aaaf59235cde930ec
René Martínez M. <sistemas5@empack.com.mx>
2025-09-22 19:17:16 -0600

    feat(all files): se agrega la pagina de lenguajes, sin emabrgo hace falta ajustar estilos y ver si se puede hacer como plantillas

--- js/mensajeInicio.js
+++ js/mensajeInicio.js
@@ -1,14 +1,16 @@
-Toastify({
-  text: "Bienvenido al Portal de Programación",
-  duration: 3000,
-  destination: "https://github.com/apvarun/toastify-js",
-  newWindow: true,
-  close: true,
-  gravity: "top", 
-  position: "center", 
-  stopOnFocus: true, 
-  style: {
-    background: "linear-gradient(to right, #00b09b, #96c93d)",
-  },
-  onClick: function(){} 
-}).showToast();
+function mensajeInicio(mensaje) {
+  Toastify({
+    text: mensaje,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {},
+  }).showToast();
+}
